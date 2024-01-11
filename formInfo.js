3c5e2bc4cd8b58db2e8dfb3b9e8ac43876c96859
msyb <msyb@itu.dk>
2024-01-11 21:29:20 +0100

    slowly working

--- formInfo.js
+++ formInfo.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log("script has been loaded");
     const submitButton = findSubmitButton();
-    submitButton.addEventListener('click', getFormInfo)
+    submitButton?.addEventListener('click', getFormInfo)
 });
 
 const findSubmitButton = () => {
@@ -10,5 +10,27 @@ const findSubmitButton = () => {
 }
 
 const getFormInfo = () => {
-    console.log("hello from the interwebs");
+    const form = document.querySelector('form');
+    const json = {};
+    json['UTM'] = getGoogleAdCookie()
+    if(!form) return;
+    
+    const inputElements = form.elements;
+    Array.from(inputElements).forEach(el => {
+        if(!el.tagName === 'INPUT') return;
+        const label = document.querySelector('label[for="' + el.id + '"]')?.textContent;
+        const trimmed = label?.toLowerCase().replace(/[^a-z]/g, '')
+        const content = el.value;
+        if(!trimmed) return;
+        json[trimmed] = content;
+    });
+    
+    console.log(json)
+}
+
+const getGoogleAdCookie = () => {
+    const currentUrl = window.location.href;
+    const cookie = currentUrl.split('?');
+    const params = cookie.length > 1 ? cookie[1] : '';
+    return params;
 }
